544973403652e88f2b68b5a3c389b08fc998871e
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-03-31 18:48:49 -0300

    2 classes criadas, falta fazê-las

---
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -0,0 +1,12 @@
+package br.edu.insper.desagil.joice.model;
+
+public class CalculadoraCCC {
+	
+	public double densidade(double peso, double raio) {
+		double avogadro = 6.023e23;
+		
+				
+	}
+	
+
+}
---
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -0,0 +1,11 @@
+package br.edu.insper.desagil.joice.model;
+
+public class CalculadoraCFC {
+	
+	public double densidade(double peso, double raio) {
+		double avogadro = 6.023e23;
+		
+	}
+		
+
+}

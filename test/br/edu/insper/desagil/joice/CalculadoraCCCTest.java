597269a64bc12fe68dda6b558f3e5633e0d44ce5
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-03-31 16:25:07 -0300

    errei

---
+++ test/br/edu/insper/desagil/joice/CalculadoraCCCTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.joice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraCCCTest {
+
+	@Test
+	void test() {
+		fail("Not yet implementedd");
+	}
+
+}
---
+++ test/br/edu/insper/desagil/joice/CalculadoraCFCTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.joice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraCFCTest {
+
+	@Test
+	void test() {
+		fail("Not yet implementedd");
+	}
+
+}
